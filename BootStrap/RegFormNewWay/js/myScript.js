66846fb0f21f6c9011e178300efa6ba64b828232
Ivaniouk <ivaniouk.e@gmail.com>
2015-10-24 15:59:58 +0300

    Failed with a script

--- BootStrap/RegFormNewWay/js/myScript.js
+++ BootStrap/RegFormNewWay/js/myScript.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    $('[name="btn_main"]').on('click', function() {
+        $('#myModal').modal('show');
+    });
+
+   // $('[name="form_Main"]').validator();
+    /*
+    $('[name="form_Main"]').validator({
+        rules: {
+            name: {
+                required: true
+            }
+        },
+        highlight: function (element) {
+            $(element).closest('.control-group').removeClass('success').addClass('error');
+        },
+        success: function (element) {
+            element.text('OK!').addClass('valid')
+                .closest('.control-group').removeClass('error').addClass('success');
+        }
+    });*/
+
 });
