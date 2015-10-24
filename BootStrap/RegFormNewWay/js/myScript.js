feaf83305e1b0ce08e91d32e9cb581a493e3a7ed
Kostya Kufta <Kostya Kufta>
2015-10-24 22:48:36 +0300

    FIX reload page after click on button

--- BootStrap/RegFormNewWay/js/myScript.js
+++ BootStrap/RegFormNewWay/js/myScript.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     $('[name="btn_main"]').on('click', function() {
         $('#myModal').modal('show');
+		return false;
     });
 
    // $('[name="form_Main"]').validator();
