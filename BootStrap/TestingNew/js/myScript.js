f8dce15a3803b5e5efe0f69c964365e4a1725857
Ivaniouk Alexander <ivaniouk.e@gmail.com>
2015-10-26 16:11:25 +0200

    New Testing init

---
+++ BootStrap/TestingNew/js/myScript.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    $('[name="btn_main"]').on('click', function() {
+        $('#myModal').modal('show');
+		return false;
+    });
+
+   // $('[name="form_Main"]').validator();
+    /*
+    $('[name="form_Main"]').validator({
+        rules: {
+            name: {
+                required: true
+            }
+        },
+        highlight: function (element) {
+            $(element).closest('.control-group').removeClass('success').addClass('error');
+        },
+        success: function (element) {
+            element.text('OK!').addClass('valid')
+                .closest('.control-group').removeClass('error').addClass('success');
+        }
+    });*/
+
+});
