f1119fa52232c45b6ba2cc6b9a9c5ef0d748f0e8
Ivaniouk <ivaniouk.e@gmail.com>
2015-10-29 19:26:53 +0200

    Added date time
    - Will fix less later

--- BootStrap/RegFormNewWay/js/myScript.js
+++ BootStrap/RegFormNewWay/js/myScript.js
@@ -4,49 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
         $('#myModal').modal('show');
 		return false;
     });*/
+    $('[name="datetimepicker"]').datetimepicker();
 
-    //$('#datetimepicker1').datetimepicker();
 
-    /*$('[name="form_Main"]').validate({
-        rules: {
-            name: {
-                minlength: 3,
-                maxlength: 15,
-                required: true
-            },
-            GameNick: {
-                minlength: 3,
-                maxlength: 15,
-                required: true
-            },
-            ForumNick: {
-                minlength: 3,
-                maxlength: 15,
-                required: true
-            },
-            email: {
-                required: true
-            },
-            conformEmail: {
-                required: true
-            },
-            password: {
-                required: true,
-                minlength: 6
-            },
-            confirmPassword: {
-                required: true,
-                minlength: 6
-            }
-        },
-        highlight: function(element) {
-            $(element).closest('.form-group').addClass('has-error');
-        },
-        unhighlight: function(element) {
-            $(element).closest('.form-group').removeClass('has-error');
-        }
-    });*/
-    //awful validation (changing forms)
-    //identical fields
-    //validation on submit ???
 });
