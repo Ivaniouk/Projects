b057779f2d3f36c22d3a3b1f4e641356bf9475d6
Ivaniouk <ivaniouk.e@gmail.com>
2015-10-30 11:17:10 +0200

    testing function for IOS

--- BootStrap/RegFormNewWay/js/myScript.js
+++ BootStrap/RegFormNewWay/js/myScript.js
@@ -8,5 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
     $('[name="datetimepicker"]').datetimepicker({
         format: 'l'
     });
+    /*
+    $(function() {
+        // Test if this is a mobile device
+        if (typeof $.browser == 'undefined') {
+            $.browser = {};
+        }
+        $.browser.device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+        if ($.browser.device) {
+            $('#arriving').attr('type', 'date');
+        } else {
+            // Datepicker for arrival date on filters
+            var dp = $('.datepicker').datepicker()
+            dp.on('changeDate', function(e) {
+                dp.datepicker('hide');
+            });
+        }
+    });*/
 
 });
