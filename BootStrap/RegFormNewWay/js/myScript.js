b6611de7f5225aaf00eed8bec0253967030a97b9
Ivaniouk <ivaniouk.e@gmail.com>
2015-10-31 01:11:52 +0200

    new submit button script

--- BootStrap/RegFormNewWay/js/myScript.js
+++ BootStrap/RegFormNewWay/js/myScript.js
@@ -1,12 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+/*
     $('[name="btn_main"]').on('click', function() {
         $('#myModal').modal('show');
 		return false;
-    });
+    });*/
+
+    $('[name="form_Main"]').validator().on('submit', function (e) {
+        if (e.isDefaultPrevented()) {
+            // handle the invalid form...
+        } else {
+            $('#myModal').modal('show');
+            return false;
+        }
+    })
+
 
     $('[name="datetimepicker"]').datetimepicker({
         format: 'l'
+    });
+
+    $(function() {
+        // Test if this is a mobile device
+        if (typeof $.browser == 'undefined') {
+            $.browser = {};
+        }
+        $.browser.device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+        if ($.browser.device) {
+            $('#arriving').attr('type', 'date');
+        } else {
+            // Datepicker for arrival date on filters
+            var dp = $('.datetimepicker').datetimepicker()
+            dp.on('changeDate', function(e) {
+                dp.datetimepicker('hide');
+            });
+        }
     });
     /*
     $(function() {
