35f4f9b236bfb2c63b86d7eea5b6a94b7aff85b6
Ivaniouk <ivaniouk.e@gmail.com>
2015-10-30 09:50:44 +0200

    Added validation
    - Fixed datetime

--- BootStrap/RegFormNewWay/js/myScript.js
+++ BootStrap/RegFormNewWay/js/myScript.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-    /*
+
     $('[name="btn_main"]').on('click', function() {
         $('#myModal').modal('show');
 		return false;
-    });*/
-    $('[name="datetimepicker"]').datetimepicker();
+    });
 
+    $('[name="datetimepicker"]').datetimepicker({
+        format: 'l'
+    });
 
 });
