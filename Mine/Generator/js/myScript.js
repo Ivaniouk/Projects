0371dcf5ab9fd8a83c4e2dab2e1d3171e23adc6b
Ivaniouk <ivaniouk.e@gmail.com>
2015-11-03 14:50:07 +0200

    Changing placeholder from code

--- Mine/Generator/js/myScript.js
+++ Mine/Generator/js/myScript.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
     $('[name="form_Main"]').on('change', function(){
         var selected = $(this).find("option:selected").val();
         $('[name="background"]').removeAttr("disabled");
-        $('[name="background"]').attr("placeholder", "Select Background");
-       //alert(WorldArr[selected]);
+        $('[name="background"]').children().first().text("Select Background")
+       //alert( $('[name="background"]:first-child'));
     });
 
 
