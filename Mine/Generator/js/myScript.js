9db2b8fe2bbed900dab89fef58f57c50a2b52774
Ivaniouk <ivaniouk.e@gmail.com>
2015-11-03 14:52:24 +0200

    update

--- Mine/Generator/js/myScript.js
+++ Mine/Generator/js/myScript.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     $('[name="form_Main"]').on('change', function(){
         var selected = $(this).find("option:selected").val();
-        $('[name="background"]').removeAttr("disabled");
-        $('[name="background"]').children().first().text("Select Background")
-       //alert( $('[name="background"]:first-child'));
+        var backgroundVar =  $('[name="background"]');
+        backgroundVar.removeAttr("disabled");
+        backgroundVar.children().first().text("Select Background")
     });
 
 
