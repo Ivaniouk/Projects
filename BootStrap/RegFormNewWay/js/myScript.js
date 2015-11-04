9ad6f30a5f3ee43f0883f6bbd29b52ade4766ad4
Ivaniouk Alexander <ivaniouk.e@gmail.com>
2015-11-04 19:33:38 +0200

    fixed a bug

--- BootStrap/RegFormNewWay/js/myScript.js
+++ BootStrap/RegFormNewWay/js/myScript.js
@@ -1,56 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-/*
+    /*
     $('[name="btn_main"]').on('click', function() {
         $('#myModal').modal('show');
 		return false;
-    });*/
+    })*/
 
-     .validator().on('submit', function (e) {
-        if (e.isDefaultPrevented()) {
+    $('[name="form_Main"]').validator().on('submit', function (event) {
+        if (event.isDefaultPrevented()) {
             // handle the invalid form...
         } else {
             $('#myModal').modal('show');
             return false;
         }
-    })
-
+    });
 
     $('[name="datetimepicker"]').datetimepicker({
         format: 'l'
     });
-    /*//not sure it's working
-    $(function() {
-        // Test if this is a mobile device
-        if (typeof $.browser == 'undefined') {
-            $.browser = {};
-        }
-        $.browser.device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
-        if ($.browser.device) {
-            $('#arriving').attr('type', 'date');
-        } else {
-            // Datepicker for arrival date on filters
-            var dp = $('.datetimepicker').datetimepicker()
-            dp.on('changeDate', function(e) {
-                dp.datetimepicker('hide');
-            });
-        }
-    });*/
-    /*
-    $(function() {
-        // Test if this is a mobile device
-        if (typeof $.browser == 'undefined') {
-            $.browser = {};
-        }
-        $.browser.device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
-        if ($.browser.device) {
-            $('#arriving').attr('type', 'date');
-        } else {
-            // Datepicker for arrival date on filters
-            var dp = $('.datepicker').datepicker()
-            dp.on('changeDate', function(e) {
-                dp.datepicker('hide');
-            });
-        }
-    });*/
-
 });
