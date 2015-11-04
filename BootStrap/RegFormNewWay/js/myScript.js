570ea4ca4fb0da1b4cbad38385b3f7b61e03da99
Ivaniouk <ivaniouk.e@gmail.com>
2015-11-04 21:38:15 +0200

    date time for IPhone

--- BootStrap/RegFormNewWay/js/myScript.js
+++ BootStrap/RegFormNewWay/js/myScript.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+$( document ).ready(function() {
     /*
     $('[name="btn_main"]').on('click', function() {
         $('#myModal').modal('show');
@@ -17,4 +17,39 @@ document.addEventListener('DOMContentLoaded', function () {
     $('[name="datetimepicker"]').datetimepicker({
         format: 'l'
     });
+
+    function check(){
+        //var isMobile = false;
+        if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+           // isMobile = true;
+            alert("Mobile");
+            /*
+             var dp = $('[name="datetimepicker"]');
+             dp.on('changeDate', function(e) {
+             dp.datepicker('hide');
+             });*/
+        }
+        else{
+            alert("PC");
+        }
+    }
+    check();
+    /*
+    $(function() {
+        // Test if this is a mobile device
+        if (typeof $.browser == 'undefined') {
+            $.browser = {};
+        }
+        $.browser.device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+        if ($.browser.device) {
+            $('#arriving').attr('type', 'date');
+        } else {
+            // Datepicker for arrival date on filters
+            var dp = $('.datepicker').datepicker()
+            dp.on('changeDate', function(e) {
+                dp.datepicker('hide');
+            });
+        }
+    });*/
+
 });
