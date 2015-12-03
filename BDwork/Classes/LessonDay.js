9a37da7aa3185bcd56cbc276e71d73f6095014c9
Ivaniouk <ivaniouk.e@gmail.com>
2015-12-03 14:41:36 +0200

    added LessonDay classs methods

--- BDwork/Classes/LessonDay.js
+++ BDwork/Classes/LessonDay.js
@@ -15,9 +15,9 @@ function LessonDay(newId, newLessonWeekId, newLessonWeekExemplar, newName, newLe
         }
 
         if (isFinite(newLessonWeekId) && Number(newLessonWeekId) > 0) {
-            this.id = parseInt(newId, 10);
+            this.lessonWeekId = parseInt(newLessonWeekId, 10);
         } else {
-            this.id = 0;
+            this.lessonWeekId = 0;
             throw new CustomPropertyError("Constructor - LessonDay newLessonWeekId is not valid", newLessonWeekId);
         }
 
@@ -40,3 +40,116 @@ function LessonDay(newId, newLessonWeekId, newLessonWeekExemplar, newName, newLe
 
     return this;
 }
+
+/** ****************METHODS*************************/
+
+LessonWeek.prototype = {
+    getID : function () {
+        return this.id;
+    },
+
+    setID : function (newId) {
+        try {
+            if (isFinite(newId) && Number(newId) > 0) {
+                this.id = parseInt(newId, 10);
+            } else {
+                throw new CustomPropertyError("Setter - LessonDay newId is not valid", newId);
+            }
+        } catch (e) {
+            // logMyErrors(e.message, e.name);
+        }
+    },
+
+    getLessonWeekId : function () {
+        return this.lessonWeekId;
+    },
+
+    setLessonWeekId : function (newLessonWeekId) {
+        try {
+            if (isFinite(newLessonWeekId) && Number(newLessonWeekId) > 0) {
+                this.lessonWeekId = parseInt(newLessonWeekId, 10);
+            } else {
+                this.lessonWeekId = 0;
+                throw new CustomPropertyError("Setter - LessonDay newLessonWeekId is not valid", newLessonWeekId);
+            }
+        } catch (e) {
+            // logMyErrors(e.message, e.name);
+        }
+
+    },
+
+    getLessonWeekExemplar : function () {
+        return this.LessonWeekExemplar;
+    },
+
+    setLessonWeekExemplar : function (newLessonWeekExemplar) {
+        try {
+            this.LessonWeekExemplar = JSON.parse(newLessonWeekExemplar); //?
+        } catch (e) {
+            // logMyErrors(e.message, e.name);
+        }
+
+    },
+
+    getName : function () {
+        return this.name;
+    },
+
+    setName : function (newName) {
+        try {
+            if (newName !== "" && newName.length <= 255 && newName.length >= 3) {
+                this.name = newName;
+            } else {
+                this.name = "unknown";
+                throw new CustomPropertyError("Setter - LessonDay newName is not valid", newName);
+            }
+        } catch (e) {
+            // logMyErrors(e.message, e.name);
+        }
+    },
+
+    getLessonMAXcount : function () {
+        return this.lessonMAXcount;
+    },
+
+    setLessonMAXcount : function (newLessonMAXcount) {
+        try {
+            if (isFinite(newLessonMAXcount) && Number(newLessonMAXcount) > 0 && Number(newLessonMAXcount) < 4) {
+                this.lessonMAXcount = parseInt(newLessonMAXcount, 10);
+            } else {
+                this.lessonMAXcount = 0;
+                throw new CustomPropertyError("Setter - LessonDay newLessonMAXcount is not valid", newLessonMAXcount);
+            }
+        } catch (e) {
+            // logMyErrors(e.message, e.name);
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
