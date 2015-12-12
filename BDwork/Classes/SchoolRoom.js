310fc45ed794f30379769a0f2c4ff115a0267941
Ivaniouk <ivaniouk.e@gmail.com>
2015-12-12 16:10:08 +0200

    School room, added work with BD

--- BDwork/Classes/SchoolRoom.js
+++ BDwork/Classes/SchoolRoom.js
@@ -8,19 +8,60 @@ function CustomPropertyError(message, property) {
 
 /** ****************CLASS*************************/
 
-function SchoolRoom(newId, newName) {
+function SchoolRoom(creationTrigger, newId, newName) {
     try {
-        if (isFinite(newId) && Number(newId) >= 100) { // Number(newId) ?
-            this.id = parseInt(newId, 10);
+        if (creationTrigger) {
+            if (isFinite(newId) && Number(newId) >= 100) {
+                this.id = parseInt(newId, 10);
+            } else {
+                throw new CustomPropertyError("Constructor - SchoolRoom ID not valid", newId);
+            }
+
+            if (newName !== "" && newName.length <= 255 && newName.length >= 3) {
+                this.name = newName;
+            } else {
+                throw new CustomPropertyError("Constructor - SchoolRoom name not valid", newName);
+            }
         } else {
-            throw new CustomPropertyError("Constructor - SchoolRoom ID not valid", newId);
+            var requestedRoom = LocalDB.SchoolRoom[newId]; // треба додати адекватний серч
+            if (requestedRoom) {
+                if (isFinite(requestedRoom.id) && Number(requestedRoom.id) >= 100) {
+                    this.id = parseInt(requestedRoom.id, 10);
+                } else {
+                    throw new CustomPropertyError("Constructor - SchoolRoom ID in LocalDB is not valid", requestedRoom.id);
+                }
+
+                if (requestedRoom.name !== "" && requestedRoom.name.length <= 255 && requestedRoom.name.length >= 3) {
+                    this.name = requestedRoom.name;
+                } else {
+                    throw new CustomPropertyError("Constructor - SchoolRoom name in LocalDB is not valid", requestedRoom.name);
+                }
+            } else {
+
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', 'api.php?controller=school_rooms&action=item&id=' + newId, false); // просто скопіював чужий запит, ще не знаю як формувати запит
+                xhr.send();
+                if (xhr.status !== 200) {
+                    throw new CustomPropertyError("Constructor - SchoolRoom by " + newId + " ID does not exist", xhr.status);
+                } else {
+                    // фішка - якщо буду перевіряти перед тим як класти в локальну базу то можна не валідувати інфу з локальної Бази на коректність ? АЛЕ! локальна База ж глобальна ?
+                    requestedRoom = JSON.parse(xhr.responseText);
+                    if (isFinite(requestedRoom.id) && Number(requestedRoom.id) >= 100) {
+                        this.id = parseInt(requestedRoom.id, 10);
+                    } else {
+                        throw new CustomPropertyError("Constructor - SchoolRoom ID in outer_lDB is not valid", requestedRoom.id);
+                    }
+
+                    if (requestedRoom.name !== "" && requestedRoom.name.length <= 255 && requestedRoom.name.length >= 3) {
+                        this.name = requestedRoom.name;
+                    } else {
+                        throw new CustomPropertyError("Constructor - SchoolRoom name in outer_DB is not valid", requestedRoom.name);
+                    }
+                    LocalDB.SchoolRoom.push(requestedRoom);
+                }
+            }
         }
 
-        if (newName !== "" && newName.length <= 255 && newName.length >= 3) {
-            this.name = newName;
-        } else {
-            throw new CustomPropertyError("Constructor - SchoolRoom name not valid", newName);
-        }
     } catch (e) {
         // logMyErrors(e.message, e.name);
         throw e;
