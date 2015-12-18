1785cc67fa23a4a7fc4ec1ee4eaf33b3eb6e830d
Ivaniouk <ivaniouk.e@gmail.com>
2015-12-18 17:14:04 +0200

    changed SchoolRoomManager class

--- BDwork/Classes/NewClasses/C_SchoolRoomManager.js
+++ BDwork/Classes/NewClasses/C_SchoolRoomManager.js
@@ -1,58 +1,50 @@
 "use strict";
-
+//TODO how to kill an instance?
 /** ****************CLASS*************************/
-function C_SchoolRoomManager() {
+function C_SchoolRoomManager(url) {
+    this._url = url;
     this._cashPool = {};
     return this;
 }
 
 /** ****************METHODS*************************/
-
 C_SchoolRoomManager.prototype = {
-    getOrCreateRoom : function (Object, trigger) {
-        var roomInstance;
-        if (trigger) {
-            roomInstance = _createRoom(Object);
-        } else {
-            roomInstance = _cashPool(Object.id);
-            if (!roomInstance) {
-                roomInstance = _loadRoom(Object.id);
-                if (roomInstance === 200) {
-                    this._cashPool[Object.id] = instance;
-                }
+    getInstance: function (Object) {
+        var Instance = _cashPool(Object.id);
+        if (!Instance) {
+            Instance = _loadInstanceById(Object.id);
+            if (Instance !== 400) {
+                this._cashPool[Object.id] = Instance;
             }
         }
-        return roomInstance;
+        return Instance;
     },
 
-    _createRoom : function (Object) {
+    createInstance: function (Object) {
         return new C_SchoolRoom(Object.id, Object.name);
     },
+    //TODO do we need this function?
+    removeInstance : function (id) {
 
-    _requestRoom : function (id) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "valid/request:" + id, true);
-        xhr.send();
-        xhr.onreadystatechange = function () {
-            return xhr;
-        };
     },
-    //TODO fix this function
-    _loadRoom : function (id) {
-        return new Promise(function (_createRoom) {
+
+    //TODO we need a timer for a request
+    _loadInstanceById : function (id) {
+        var url = this._url;
+        return new Promise(function (createInstance) {
             var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'valid/request:' + id, false)
+            xhr.open('GET', url + "request" + id, false);
             xhr.send();
             if (xhr.status === 200) {
-                _createRoom(JSON.parse(xhr.responseText));
+                createInstance(JSON.parse(xhr.responseText));
             }
             return xhr.status;
         });
     },
 
-    _loadAllRooms : function () {
+    _loadAllInstances : function () {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "valid/request:ALL", true);
+        xhr.open("GET", this._url + "request:ALL", true);
         xhr.send();
         xhr.onreadystatechange = function () {
             return JSON.parse(xhr.responseText);
@@ -60,17 +52,21 @@ C_SchoolRoomManager.prototype = {
         return xhr.status;
     },
 
-    _saveToOuterBase : function (roomObject) {
-        this._cashPool[roomObject.roomId] = _createRoom(roomObject);
-        var jsonRoom = JSON.stringify(roomObject);
+    _saveToOuterBase : function (Object) {
+        this._cashPool[Object.id] = createInstance(Object);
+        var jsonObj = JSON.stringify(Object);
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", 'valid/POST', true);
+        xhr.open("POST", this._url + "valid/POST", true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.onreadystatechange = function () {
             if (this.readyState !== 200) {
                 return xhr.status;
             }
         };
-        xhr.send(jsonRoom);
+        xhr.send(jsonObj);
+    },
+    //TODO Remove object from pool??
+    _removeFromOuterBase : function (id) {
+
     }
 };
