4eb07ae80603f5ccff818503f30400b7b2904fb0
Ivaniouk <ivaniouk.e@gmail.com>
2015-12-22 00:45:50 +0200

    RoomManager new methods

--- BDwork/Classes/NewClasses/RoomManager.js
+++ BDwork/Classes/NewClasses/RoomManager.js
@@ -19,7 +19,7 @@ C_SchoolRoomManager.prototype = {
         var Instance = _cashPool(object.id);
         if (!Instance) {
             Instance = _loadInstanceById(object.id);
-            if (Instance !== 400) {
+            if (Instance !== 400) { // add network errors status
                 this._cashPool[object.id] = Instance;
             }
         }
@@ -30,12 +30,71 @@ C_SchoolRoomManager.prototype = {
     createRoom : function (name) {
         return _saveInstanceByName(name);
     },
+    /** Returns promise to delete a room*/
+    deleteRoom : function (object) {
+        return _deleteFromServerBase(object.id);
+    },
+    /** Returns promise to load all rooms*/
+    getAllRooms : function () { //private or public ?
+        return _getAllRooms();
+    },
+
+    /** Kills instance in the pool*/
+    _deleteFromPool : function (id) {
+        delete this._cashPool[id]; // ≥ тут € забув €к ти по€снював про видаленн€ (дуже п≥ч€льний смайл)
+    },
+    /**POST. Sends ID to server -> Server looks for instance with this ID -> server returns result*/
+    _deleteFromServerBase : function (id) {
+        var url = this._url;
+        return new Promise(function (_deleteFromPool, LogServerFailures) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', url + "/request:" + id, false);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onload = function () {
+                if (this.status >= 200 && this.status < 300) {
+                    _deleteFromPool(id);
+                } else {
+                    LogServerFailures(xhr.status);
+                }
+            };
+            xhr.onerror = function () {
+                LogServerFailures(xhr.status);
+            };
+            xhr.send(""); // Empty POST for deletion -> то ¬алентин так сказав € н≥ при чому! =)
+        });
+    },
+
     /** Creates new room -> adds it to the _cashPool -> returns Room instance */
     _createInstance : function (object) {
         var instance = new C_SchoolRoom(object.id, object.name);
         this._cashPool[object.id] = instance;
         return instance;
     },
+
+    _loadPool : function (object) {
+        //брутальне глибоке коп≥юванн€ об"Їкт≥в €ке € не знаю €к робить
+    },
+
+    /**GET. Request all rooms on the server -> server sends back all rooms */
+    _getAllRooms : function () {
+        var url = this._url;
+        return new Promise(function (_loadPool, LogServerFailures) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url + "request", false);
+            xhr.onload = function () {
+                if (this.status >= 200 && this.status < 300) {
+                    _loadPool(JSON.parse(xhr.responseText));
+                } else {
+                    LogServerFailures(xhr.status);
+                }
+            };
+            xhr.onerror = function () {
+                LogServerFailures(xhr.status);
+            };
+            xhr.send();
+        });
+    },
+
     /** POST. Sends name to server -> Server saves adding ID -> server returns Room object with ID*/
     _saveInstanceByName : function (name) {
         var url = this._url;
@@ -56,7 +115,7 @@ C_SchoolRoomManager.prototype = {
             xhr.send(JSON.stringify(name));
         });
     },
-    /** GET. Sends ID to server -> Server looks for instance with this ID -> server returns result*/ //попередити хлопц≥в шоб слали 400 €кшо не нашли в баз≥ ID
+    /** GET. Sends ID to server -> Server looks for instance with this ID -> server returns result*/
     _loadInstanceById : function (id) {
         var url = this._url;
         return new Promise(function (_createInstance, LogServerFailures) {
