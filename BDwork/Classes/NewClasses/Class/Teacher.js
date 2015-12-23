69c262b9e4173285c9c703c23548d87b6b62fcaf
Ivaniouk <ivaniouk.e@gmail.com>
2015-12-23 13:23:43 +0200

    added new teacher Class

--- BDwork/Classes/NewClasses/Class/Teacher.js
+++ BDwork/Classes/NewClasses/Class/Teacher.js
@@ -2,14 +2,14 @@
 
 /** ****************CLASS*************************/
 
-function BaseClass(newId, newName) {
-    _CreateClass(newId, newName);
+function Teacher(newId, newName, newMidName, newLastName) {
+    _CreateClass(newId, newName, newMidName, newLastName);
     return this;
 }
 
 /** ****************METHODS*************************/
 
-BaseClass.prototype = {
+Teacher.prototype = {
     getID : function () {
         return this.id;
     },
@@ -26,11 +26,35 @@ BaseClass.prototype = {
         return this.name;
     },
 
+    getMidName : function () {
+        return this.midName;
+    },
+
+    getLastName : function () {
+        return this.LastName;
+    },
+
     setName : function (newName) {
         if (_validateName(newName)) {
             this.name = newName;
         } else {
             logMyErrors("Setter " + this.constructor.name + " name not valid", newName);
+        }
+    },
+
+    setMidName : function (newMidName) {
+        if (_validateName(newMidName)) {
+            this.name = newMidName;
+        } else {
+            logMyErrors("Setter " + this.constructor.name + " MidName not valid", newMidName);
+        }
+    },
+
+    setLastName : function (newLastName) {
+        if (_validateName(newLastName)) {
+            this.name = newLastName;
+        } else {
+            logMyErrors("Setter " + this.constructor.name + " LastName not valid", newLastName);
         }
     },
 
@@ -42,7 +66,7 @@ BaseClass.prototype = {
         return (newName !== "" && newName.length <= 255 && newName.length >= 3);
     },
 
-    _CreateClass : function (newId, newName) {
+    _CreateClass : function (newId, newName, newMidName, newLastName) {
         if (_validateID(newId)) {
             this.id = parseInt(newId, 10);
         } else {
@@ -54,6 +78,18 @@ BaseClass.prototype = {
             this.name = newName;
         } else {
             logMyErrors("Constructor - " + this.constructor.name + " name not valid", newName);
+        }
+
+        if (_validateName(newMidName)) {
+            this.midName = newMidName;
+        } else {
+            logMyErrors("Constructor - " + this.constructor.name + " MidName not valid", newMidName);
+        }
+
+        if (_validateName(newLastName)) {
+            this.LastName = newLastName;
+        } else {
+            logMyErrors("Constructor - " + this.constructor.name + " LastName not valid", newLastName);
         }
     }
 };
