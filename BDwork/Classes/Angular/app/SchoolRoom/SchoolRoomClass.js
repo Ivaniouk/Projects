a9ef72a88bfc8b48f9204a58225fd0ff0239d6cd
Ivaniouk <ivaniouk.e@gmail.com>
2016-01-07 12:31:38 +0200

    angular

---
+++ BDwork/Classes/Angular/app/SchoolRoom/SchoolRoomClass.js
@@ -0,0 +1,59 @@
+'use strict';
+
+function SchoolRoom(object) {
+    if (object) {
+        _CreateClass(object.id, object.name);
+    }
+    //angular.extend(this, schoolRoomData);
+    SchoolRoom.prototype = {
+        getID : function () {
+            return this.id;
+        },
+
+        setID : function (newId) {
+            if (_validateID(newId)) {
+                //angular.extend(this.id, parseInt(newId, 10));
+                //this.id = parseInt(newId, 10);
+            } else {
+                logMyErrors("Setter " + this.constructor.name + " ID not valid", newId);
+            }
+        },
+
+        getName : function () {
+            return this.name;
+        },
+
+        setName : function (newName) {
+            if (_validateName(newName)) {
+                this.name = newName;
+            } else {
+                logMyErrors("Setter " + this.constructor.name + " name not valid", newName);
+            }
+        },
+
+        _validateID : function (newId) {
+            return (isFinite(newId));
+        },
+
+        _validateName : function (newName) {
+            return (newName !== "" && newName.length <= 255 && newName.length >= 3);
+        },
+
+        _CreateClass : function (newId, newName) {
+            if (_validateID(newId)) {
+                this.id = parseInt(newId, 10);
+            } else {
+                logMyErrors("Constructor - " + this.constructor.name + " ID not valid", newId);
+                return;
+            }
+
+            if (_validateName(newName)) {
+                this.name = newName;
+            } else {
+                logMyErrors("Constructor - " + this.constructor.name + " name not valid", newName);
+            }
+        }
+    };
+
+    return SchoolRoom;
+}
