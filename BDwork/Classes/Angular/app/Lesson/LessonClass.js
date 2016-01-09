2d7b7a80f294c82e58f6691e6a0fd9fd62b65680
Ivaniouk <ivaniouk.e@gmail.com>
2016-01-09 14:11:01 +0200

    added set/get

--- BDwork/Classes/Angular/app/Lesson/LessonClass.js
+++ BDwork/Classes/Angular/app/Lesson/LessonClass.js
@@ -11,8 +11,56 @@ function Lesson(object) {
             return thisClass.id;
         },
 
+        getLessonDayID : function () {
+            return thisClass.lessonDayID;
+        },
+
+        getLessonNumber : function () {
+            return thisClass.lessonNumber;
+        },
+
+        getSchoolClassID : function () {
+            return thisClass.schoolClassID;
+        },
+
+        getSubjectID : function () {
+            return thisClass.subjectID;
+        },
+
+        getSchoolRoomID : function () {
+            return thisClass.schoolRoomID;
+        },
+
+        getTeacherID : function () {
+            return thisClass.teacherID;
+        },
+
         setID : function (newId) {
             thisClass.id = parseInt(newId, 10);
+        },
+
+        setLessonDayID: function (newId) {
+            thisClass.lessonDayID = parseInt(newId, 10);
+        },
+
+        setLessonNumber: function (newId) {
+            thisClass.lessonNumber = parseInt(newId, 10);
+        },
+
+        setSchoolClassID: function (newId) {
+            thisClass.schoolClassID = parseInt(newId, 10);
+        },
+
+        setSubjectID: function (newId) {
+            thisClass.subjectID = parseInt(newId, 10);
+        },
+
+        setSchoolRoomID: function (newId) {
+            thisClass.schoolRoomID = parseInt(newId, 10);
+        },
+
+        setTeacherID: function (newId) {
+            thisClass.teacherID = parseInt(newId, 10);
         },
 
         _CreateClass : function (object) {
