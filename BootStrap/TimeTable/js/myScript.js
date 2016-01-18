9a6d9198890b7826759cf3b797626b87111c6337
Ivaniouk <ivaniouk.e@gmail.com>
2016-01-18 18:08:38 +0200

    timetable start

---
+++ BootStrap/TimeTable/js/myScript.js
@@ -0,0 +1,6 @@
+function sideBarsControl($scope){
+    $scope.daysArr = ["Monday", "Tuesday", "Wednesday"];
+    $scope.daysNumberArr = ["0", "1", "2", "3", "4", "5", "6", "7"];
+    $scope.ClassesSidebar = ["6 - a", "6 - a", "7 - a", "8 - a", "9 - a", "11 - a", "11 - a"];
+
+}
