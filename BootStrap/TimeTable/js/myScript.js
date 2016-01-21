19fc98229b25b4dd704af1c2de22c88cc2daee89
Ivaniouk <ivaniouk.e@gmail.com>
2016-01-21 14:43:04 +0200

    fixed ololo bug

--- BootStrap/TimeTable/js/myScript.js
+++ BootStrap/TimeTable/js/myScript.js
@@ -1,8 +1,8 @@
 
-function sideBarsControl($scope){
+function SideBarsController($scope){
     $scope.daysArr = ["Monday", "Tuesday", "Wednesday"];
     $scope.daysNumberArr = ["0", "1", "2", "3", "4", "5", "6", "7"];
-    $scope.ClassesSidebarArr = ["6 - a", "6 - a", "7 - a", "8 - a", "9 - a", "11 - a", "11 - a"];
+    $scope.classesSidebarArr = ["6 - a", "6 - б", "7 - a", "8 - a", "9 - a", "11 - a", "12 - a"];
 }
 
 function SimpleDemoController($scope){
@@ -10,32 +10,39 @@ function SimpleDemoController($scope){
         selected: null,
         lists: {"A": [
             {
+                "label": "Item A1"
+            },
+            {
                 "label": "Item A2"
             },
             {
                 "label": "Item A3"
             },
             {
-                "label": "Item B3"
+                "label": "Item A4"
             }
         ], "B": [
             {
                 "label": "Item B1"
             },
             {
-                "label": "Item A1"
+                "label": "Item B2"
             },
             {
-                "label": "Item B2"
+                "label": "Item B3"
+            },
+            {
+                "label": "Item B4"
             }
         ]}
     };
 
     // Generate initial model
+    /*
     for (var i = 1; i <= 3; ++i) {
         $scope.models.lists.A.push({label: "Item A" + i});
         $scope.models.lists.B.push({label: "Item B" + i});
-    }
+    }*/
 
     // Model to JSON for demo purpose
     $scope.$watch('models', function(model) {
@@ -43,58 +50,12 @@ function SimpleDemoController($scope){
     }, true);
 }
 
-angular
-    .module('demo', [])
-    .controller('sideBarsControl', sideBarsControl);
+
+
 angular
     .module('demo', ["dndLists"])
+    .controller('SideBarsController', SideBarsController)
     .controller('SimpleDemoController', SimpleDemoController);
 
-
-/*
-angular.module("demo").controller("SimpleDemoController", function($scope) {
-    $scope.models = {
-        selected: null,
-        lists: {"A": [
-            {
-                "label": "Item A2"
-            },
-            {
-                "label": "Item A3"
-            },
-            {
-                "label": "Item B3"
-            }
-        ], "B": [
-            {
-                "label": "Item B1"
-            },
-            {
-                "label": "Item A1"
-            },
-            {
-                "label": "Item B2"
-            }
-        ]}
-    };
-
-    // Generate initial model
-    for (var i = 1; i <= 3; ++i) {
-        $scope.models.lists.A.push({label: "Item A" + i});
-        $scope.models.lists.B.push({label: "Item B" + i});
-    }
-
-    // Model to JSON for demo purpose
-    $scope.$watch('models', function(model) {
-        $scope.modelAsJson = angular.toJson(model, true);
-    }, true);
-
-});
-
-angular.module("demo").controller("sideBarsControl", function($scope){
-    $scope.daysArr = ["Monday", "Tuesday", "Wednesday"];
-    $scope.daysNumberArr = ["0", "1", "2", "3", "4", "5", "6", "7"];
-    $scope.ClassesSidebarArr = ["6 - a", "6 - a", "7 - a", "8 - a", "9 - a", "11 - a", "11 - a"];
-});*/
 
 
